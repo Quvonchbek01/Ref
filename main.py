41132c69e9764b710f0c65eda076bda5ebe066f7
Xlertuzb <122796543+Quvonchbek01@users.noreply.github.com>
2025-02-23 11:27:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start", "hack"))
-async def start_cmd(message: Message):
+async def start_cmd(message):
     await message.answer(f"👋 Welcome, {message.from_user.first_name}!\n Solve the following riddle and join Cracking World!\nWhat is something that is both valuable and worthless at the same time?")
 
 @dp.message()
-async def kurs_handler(message: Message):
+async def kurs_handler(message):
     if message.text.strip().lower() == ["vaqt", "time"] :
         user_id = str(message.from_user.id)
         user_name = message.from_user.first_name
