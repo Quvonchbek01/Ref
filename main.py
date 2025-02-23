a07c886b8c21bf7c99657cfc6cc966f3461193d9
Xlertuzb <122796543+Quvonchbek01@users.noreply.github.com>
2025-02-23 11:23:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -20,18 +20,18 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start", "hack"))
-async def start_cmd(message):
+async def start_cmd(message: Message):
     await message.answer(f"👋 Welcome, {message.from_user.first_name}!\n Solve the following riddle and join Cracking World!\nWhat is something that is both valuable and worthless at the same time?")
 
 @dp.message()
-async def data_handler(message):
-    if message.text.strip().lower() == "vaqt" or message.text.strip().lower() == "time" :
+async def data_handler(message: Message):
+    if message.text.strip().lower() == ["vaqt", "time"] :
         user_id = str(message.from_user.id)
         user_name = message.from_user.first_name
 
         invite_link = await get_invite(user_id)  # **Bazadan tekshiramiz**
         if invite_link:
-            await message.answer(f"Don't hack ! \nYou have already been given the link.\n🔗 {invite_link}")
+            await message.answer(f"Don't hack! \nYou have already been given the link.\n🔗 {invite_link}")
         else:
             try:
                 new_invite: ChatInviteLink = await bot.create_chat_invite_link(
